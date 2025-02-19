5ddf87cf10bd59bbfbc54b876fd2d9c48cacc936
qf3l3k <qf3l3k@gmail.com>
2025-02-19 14:50:11 +0100

    Added exporter and logger module.

--- src/cert_exp_check/__main__.py
+++ src/cert_exp_check/__main__.py
@@ -1,25 +1,37 @@
-import sys
-
 from cert_exp_check.checker import check_certificates
 from cert_exp_check.cli import process_command_line
+from cert_exp_check.config import load_config
+from cert_exp_check.exporter import run_exporter
+from cert_exp_check.logger import setup_logger
 
 
-def main():
-    input_file, logging = process_command_line()
-    url_list = []
-    try:
-        with open(input_file) as file:
-            for line in file:
-                url_list.append(line.rstrip())
-    except IOError:
-        sys.exit("Incorrect file name with list of URLs.")
-    check_results, check_errors = check_certificates(url_list, input_file, logging)
+def cli_mode(config, log_level):
+    """Runs the CLI mode"""
+    url_list = [item['url'] for item in config.get('urls', [])]
+    check_results, check_errors = check_certificates(url_list, 'config.yaml', log_level)
     print(check_results)
-    if logging == 'verbose':
+    if check_errors:
         print('===')
         print("Errors during check")
         print('-')
         print(check_errors)
+
+
+def main():
+    working_mode, config_path, log_level, log_file_path = process_command_line()
+    print(working_mode, config_path, log_level, log_file_path)
+    logger = setup_logger(log_level=log_level, log_file=log_file_path)
+
+    logger.info(f'working_mode: {working_mode}, config_path: {config_path}, log_level: {log_level}')
+    config = load_config(config_path)
+    logger.debug(f'config: {config}')
+
+    if working_mode == 'exporter':
+        logger.info("Starting exporter mode...")
+        run_exporter(config)
+    elif working_mode == 'cli':
+        logger.info("Starting CLI mode...")
+        cli_mode(config, log_level)
 
 
 if __name__ == '__main__':
--- src/cert_exp_check/cli.py
+++ src/cert_exp_check/cli.py
@@ -9,26 +9,40 @@ def process_command_line():
     cmdparser = argparse.ArgumentParser(prog='cert_check',
                                         usage='%(prog)s [options] path',
                                         description='Checks when certificate for specified hosts expires.')
-    cmdparser.version = "0.1"
+    cmdparser.version = "0.2"
 
-    cmdparser.add_argument("-f", "--file",
+    cmdparser.add_argument("--mode",
                            type=str,
-                           nargs=1,
                            action="store",
-                           dest="file",
+                           dest="mode",
+                           choices=['cli', 'exporter'],
+                           required=True,
+                           help="Mode to run the application")
+
+    cmdparser.add_argument("--config",
+                           type=str,
+                           action="store",
+                           dest="config",
                            required=True,
                            help="specifies file with list of URLs to check")
 
-    cmdparser.add_argument("-l", "--log",
+    cmdparser.add_argument("--log-level",
                            type=str,
-                           nargs=1,
                            action="store",
-                           dest="log",
+                           dest="log_level",
                            choices=["info", "verbose"],
                            required=True,
-                           help="defines error logging level")
+                           help="defines logging level")
+
+    cmdparser.add_argument("--log-file",
+                           type=str,
+                           action="store",
+                           dest="log_file",
+                           required=False,
+                           default="cert_exp_check.log",
+                           help="defines logging file")
 
     cmdparser.add_argument("-v", "--version", action="version")
     args = cmdparser.parse_args()
 
-    return args.file[0], args.log[0]
+    return args.mode, args.config, args.log_level, args.log_file
